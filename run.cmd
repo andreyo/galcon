105b7289443e2adf00f52a48ebc6cd0a5ce25dfd
andrey <aosipov.me@gmail.com>
2012-06-03 23:21:59 +0300

    galcon

---
+++ run.cmd
@@ -0,0 +1,1 @@
+java -Duser.language=en -jar tools/PlayGame.jar maps/map7.txt 1000 1000 log.txt "java -cp target/classes com.galcon.MyBot" "java -cp target/classes com.galcon.MyBot2" | java -jar tools/ShowGame.jar
